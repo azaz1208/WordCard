3d85923ab0da78ff6d507c305cb1456e2a56aaa9
azaz1208 <angela87120837@gmail.com>
2024-09-21 23:37:29 +0800

    讓新增的字卡可以顯示word 20240921 23:36

--- main.py
+++ main.py
@@ -6,15 +6,20 @@ from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDFlatButton
 from kivy.core.text import LabelBase
+import uuid
 
 Window.size = (400, 600)
 
 class WordCard_layout(MDBoxLayout):
     text = StringProperty("")
+    card_id = StringProperty("")
 class AddCardDialog_Content(MDBoxLayout):
     pass
 
 class MainApp(MDApp):
+    # 初始化一個字典用來儲存使用者存的資料
+    sum_dict = {}
+
     # buid MainApp (白話：用kivyMD就要照他的規則，所以一定得寫build)
     # build 可以決定整個 APP 的視窗內容長什麼樣子
     # return 什麼，就會長什麼樣子
@@ -27,10 +32,22 @@ class MainApp(MDApp):
 
 
     # 設定當我按下某個btn的時候，我的APP視窗要增加一行 字卡格子
+    # 同時還要把使用者打的資料存起來
     def add_card(self,*arg):
-        self.getTextInput()
-        new_card = WordCard_layout(text = "New Card!")
-        self.root.ids.wordCard_area.add_widget(new_card)
+        newCardData = self.getTextInput()
+        if newCardData["word"] != "":
+            # generate an unique id for every card
+            card_id = str(uuid.uuid4())
+            
+            new_card = WordCard_layout(text = newCardData["word"], card_id = card_id)
+            self.root.ids.wordCard_area.add_widget(new_card)
+
+            # 存資料
+            self.save_card_data(card_id, newCardData)
+
+    def save_card_data(self, card_id, data):
+        self.sum_dict[ card_id ] = data
+        print(f'Data recorded: {self.sum_dict}')
 
     # 關閉彈出視窗的效果
     def close_dialog(self, *args):
@@ -63,12 +80,13 @@ class MainApp(MDApp):
         sentence = self.dialog.content_cls.ids.sentence_input.text
         
         self.close_dialog() 
-        
+       
         data = {
             "word": word,
             "meaning": meaning,
             "sentence": sentence
         }
+        
         print(data)
         return data
         
