0907fa455ed0bf9fc5711589ac2fb95cb389f1b2
azaz1208 <angela87120837@gmail.com>
2024-10-07 18:15:58 +0800

    增加edit, next, last, mdicon 20241007 18:14

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ Window.size = (400, 600)
 class WordCard_layout(MDBoxLayout):
     text = StringProperty("")
     card_id = StringProperty("")
+class Plus_WordCard_layout(MDBoxLayout):
+    text = StringProperty("")
+    card_id = StringProperty("")
 class AddCardDialog_Content(MDBoxLayout):
     pass
 
@@ -25,7 +28,7 @@ class MainApp(MDApp):
     # build 可以決定整個 APP 的視窗內容長什麼樣子
     # return 什麼，就會長什麼樣子
     def build(self):  
-        LabelBase.register(name="MSJH", fn_regular="./fonts/MSJH.TTC")
+        LabelBase.register(name="MSJH", fn_regular="./fonts/blackH1.otf")
 
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Blue"
@@ -107,15 +110,15 @@ class MainApp(MDApp):
             buttons = [
                 MDFlatButton(
                     text = "Last",
-                    on_release = self.close_dialog
+                    on_release = lambda x : self.show_last_card_dialog(card_id)
                 ),
                 MDFlatButton(
                     text = "Next",
-                    on_release = self.close_dialog
+                    on_release = lambda x : self.show_next_card_dialog(card_id)
                 ),
                 MDFlatButton(
                     text = "Edit",
-                    on_release = self.close_dialog
+                    on_release = lambda x : self.show_edit_card_dialog(card_id)
                 ),
                 MDFlatButton(
                     text = "Close",
@@ -158,6 +161,121 @@ class MainApp(MDApp):
         content.add_widget(sentence)
 
         return content
+    def show_edit_card_dialog(self,card_id):
+        # close any open dialog first
+        if self.dialog:
+            self.dialog.dismiss()
+            self.dialog = None
+
+        # 取得原有字卡的資料
+        card_data = self.sum_dict.get(card_id,{})
+
+        # 如果找不到資料就不做任何事
+        if not card_data:
+            return
+
+        # 產生一個已經填好原有資料的內容
+        content = AddCardDialog_Content()
+        content.ids.word_input.text = card_data.get("word", "")
+        content.ids.meaning_input.text = card_data.get("meaning", "")
+        content.ids.sentence_input.text = card_data.get("sentence", "")
+
+
+        self.dialog = MDDialog(
+            title = "Edit word",
+            type = "custom",
+            content_cls = content,
+            buttons = [
+                MDFlatButton(
+                    text="CANCEL",
+                    # theme_text_color="Custom",
+                    # text_color=self.theme_cls.primary_color,
+                    on_release = self.close_dialog
+                ),
+                MDFlatButton(
+                    text="SAVE",
+                    # theme_text_color="Custo
+                    on_release = lambda x :self.save_edited_card(card_id)
+                )
+            ],
+        )
+
+        self.dialog.open()
+    def save_edited_card(self,card_id):
+
+        # get the data user edited
+        word_card = self.getTextInput()
+
+        # update sum_dict 
+        self.sum_dict[card_id] = word_card
+
+        # update wordcard label
+        for card in self.root.ids.wordCard_area.children:
+            if card.card_id == card_id:
+                card.text = word_card.get("word","")
+                break
+        print(f"card Data updated: {self.sum_dict}")
+
+        self.close_dialog()
+        self.dialog = None
+        self.on_wordCard_label_press(card_id)
+    def show_next_card_dialog(self,card_id):
+        if self.dialog:
+            self.dialog.dismiss()
+            self.dialog = None
+
+        # 先取得所有的card_id
+        all_card_id = list(self.sum_dict.keys())
+
+        if all_card_id == []:
+            return
+
+        # 找出當前這張卡是第幾張
+        current_card_index = all_card_id.index(card_id)
+
+        # (狀況一：當前的卡是最後一張)
+        if current_card_index == len(all_card_id)-1:
+            next_card_index = 0
+        else:
+            # 找出下一張 (狀況二：當前的卡不是最後一張)
+            next_card_index = current_card_index +1 
+
+        # 找出下一張卡的card id
+        next_card_id = all_card_id[next_card_index]
+
+        self.on_wordCard_label_press(next_card_id)
+
+    def show_last_card_dialog(self,card_id):
+        if self.dialog:
+            self.dialog.dismiss()
+            self.dialog = None
+
+        # 先取得所有的card_id
+        all_card_id = list(self.sum_dict.keys())
+
+        if all_card_id == []:
+            return
+
+        # 找出當前這張卡是第幾張
+        current_card_index = all_card_id.index(card_id)
+
+        # (狀況一：當前的卡是第一張)
+        if current_card_index == 0 :
+            last_card_index = -1
+        else:
+            # 找出上一張 (狀況二：當前的卡不是第一張)
+            last_card_index = current_card_index -1 
+
+        # 找出上一張卡的card id
+        last_card_id = all_card_id[last_card_index]
+
+        self.on_wordCard_label_press(last_card_id)
+
+
+
+
+
+
 
 
 
